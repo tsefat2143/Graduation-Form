b68453b8cc44b98ae0172d9c7dc74c57b97333c5
tsefat2143 <tajdidul.sefat@gmail.com>
2021-01-18 01:30:09 -0500

    Fixed Passwords

--- JavaScript/routes/register.js
+++ JavaScript/routes/register.js
@@ -2,17 +2,18 @@ let router = require("express").Router();
 let bodyParser = require("body-parser");
 let database = require('../database');
 let flash = require('connect-flash');
-let cookieParser = require('cookie-parser');
 let session = require('express-session');
-let path = require('path');
 let bcrypt = require('bcrypt');
+require("dotenv").config();
 
-router.use(cookieParser('secret'));
+//Express Session
 router.use(session({
+	secret:process.env.SESSION_SECRET,
 	cookie:{maxAge:60000},
 	resave: false,
 	saveUninitialized:false
 }));
+
 router.use(flash());
 
 router.use(bodyParser.urlencoded({extended: false,}));
@@ -24,25 +25,21 @@ router.post("/register", (req, res) => {
 	let last = req.body.last;
 	let middle = req.body.middle;
 	let username = req.body.username;
-	let password = req.body.password;
-	let password2 = req.body.password2;
+	let password =  bcrypt.hashSync(req.body.password,10);
 	let email = req.body.email;
 	let gender = req.body.gender;
 	let dob = req.body.dob;	
-
-	if(password === password2)
-		console.log("Equal Passwords");
-	else console.log("FAAAAAAALSE");
 
 	let sql = `Insert Into graduation.graduates(firstName,lastName,middleName,username,passwords,email,gender,DOB) 
 	Values('${first}','${last}','${middle}','${username}','${password}','${email}','${gender}','${dob}');`;
 
 	database.query(sql, (error, result) => {
-		if (error) console.log(error);
+		if (error) {
+			console.log(error);
+		}
 		else {
 			console.log("SQL Record Inserted");
 			req.flash('message', 'Success!!');
-
 			res.redirect('/complete');
 		}
 	});
